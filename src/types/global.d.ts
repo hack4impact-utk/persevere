fc4ed9af35b754519b740d459e53089f3bbf2a88
kvnguo <kevinguo1011@gmail.com>
2025-10-14 18:21:57 -0400

    feat: remove Google OAuth and use in-house authentication only

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,7 +4,5 @@ declare namespace NodeJS {
   interface ProcessEnv {
     DATABASE_URL: string;
     NEXTAUTH_SECRET: string;
-    GOOGLE_CLIENT_ID: string;
-    GOOGLE_CLIENT_SECRET: string;
   }
 }
