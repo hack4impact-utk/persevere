103711eb0605290a984962cdbda21ba1cb6a6ea1
Vincent Guo <152289662+guo-vincent@users.noreply.github.com>
2025-10-26 23:29:39 -0400

    Revert staff/dashboard/page.tsx for PR

--- src/app/staff/dashboard/page.tsx
+++ src/app/staff/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import { Box } from "@mui/material";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 import { JSX } from "react";
 
 import authOptions from "@/app/api/auth/[...nextauth]/auth-options";
 import { StaffDashboardContent } from "@/components";
-import StaffHeader from "@/components/layout/staff-header";
 
 export default async function StaffDashboardPage(): Promise<JSX.Element> {
   const session = await getServerSession(authOptions);
@@ -14,10 +12,5 @@ export default async function StaffDashboardPage(): Promise<JSX.Element> {
     redirect("/auth/login");
   }
 
-  return (
-    <Box>
-      <StaffHeader />
-      <StaffDashboardContent />
-    </Box>
-  );
+  return <StaffDashboardContent />;
 }
